2101af740d6f86d1493f67fe8a2dadc284c8e2c7
Hanna Pokamiestova <105422641+HannaPokamiestova@users.noreply.github.com>
2023-09-12 19:52:58 +0200

    update filter, contact form, fix package.json

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -6,6 +6,29 @@ export const FilterStyled = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 12 px;
-  margin-bottom: 16 px;
+  gap: 12px;
+  margin-bottom: 16px;
+`;
+
+export const Label = styled.label`
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 1.16;
+`;
+
+export const Input = styled.input`
+  font-size: 18px;
+  line-height: 1.16;
+  width: 300px;
+  height: 26px;
+  border: 2px solid #212121;
+  border-radius: 4px;
+  outline: none;
+  padding: 8px 16px;
+  transition: border-color 250ms linear;
+
+  :hover,
+  :focus {
+    border-color: orange;
+  }
 `;
